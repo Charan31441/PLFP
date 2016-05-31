4fd59348db18deb84292705243e05c4b9ff64e76
Charan31441 <charan.regunta@gmail.com>
2016-06-01 05:24:26 +0530

    Update py_interpreter.py

--- Adventure/py_interpreter.py
+++ Adventure/py_interpreter.py
@@ -57,66 +57,7 @@ def store(variable,value):
 			break
 	Values[variable]=float(value)
 ########################################################################################################################################
-"""	
-def Interpreter_calc(text):
-	left=0
-        right=0	
-	count=0
-	if text[0]== '-':
-		s=-1
-		text=text[1:]
-	else:
-		s=1
-        for i in text:
-        	try:
-        	        left = (left*10)+float(i)
-        	        count=count+1
-		except:
-			if count == 0:
-				print i
-				left=Values[i]
-				count=count+1
-        		break
-	flag=1	
-	if text[count] == '*':
-		flag=0
-		if text[count+1] == '*':
-			op = '^'
-			count = count+1
-		else:
-			
-			op=text[count]
-	if flag:
-		op=text[count]	
-	if count > 0:
-        	right=0
-        	f=0
-        	index=count+1
-        	for i in range(index,len(text)):
-            		try:
-                		right=right*10+float(text[i])
-                		f=f+1
-            		except:
-				if f == 0:	
-					right=Values[text[i]]
-					f=f+1
-                			break
-        	left=left*s
-	left_value=left
-	right_value=right	
-	if op == '+': 
-		result = left_value + right_value
-	elif op == '-': 
-		result = left_value - right_value
-	elif op == '*': 
-		result = left_value * right_value
-	elif op == '/': 
-		result = left_value / right_value
-	elif op == '^': 
-		result = left_value ** right_value
-	elif op == '%': 
-		result = left_value % right_value
-	return round(float(result),3)"""
+
 ########################################################## CALCULATION(Replacing Variables with Values)##################################
 def calculation(text):
 	#result=Interpreter_calc(text)			
